6fba7e2229584a6a452075c287ee0aac80290223
MarieCamillePetit <76949730+MarieCamillePetit@users.noreply.github.com>
2021-11-27 16:26:43 +0100

    Phase 3 : introduction à Twig et simplification du contrôleur.

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,21 +8,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * Description of AccueilController
  *
- * @author petit
+ * @author Mc
  */
-class AccueilController {
+class AccueilController extends AbstractController {
     /**
      * @Route("/", name="acceuil")
      * @return Response
      */
     public function index(): Response {
         
-        return new Response('Hello world!');
+        return $this->render("pages/accueil.html.twig");
         
     }
+    
 }
