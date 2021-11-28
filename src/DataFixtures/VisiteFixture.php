cc958e8228ea36d1af2be642cde9e7b36584da75
MarieCamillePetit <76949730+MarieCamillePetit@users.noreply.github.com>
2021-11-28 10:17:29 +0100

    Phase 7 : ORM (génération automatique d'enregistrements).

---
+++ src/DataFixtures/VisiteFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Visite;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class VisiteFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // création du faxer pour la génération des valeurs aléatoires
+        $faker = Factory::create('fr_FR');
+        //génération des enregistrements
+        for($k=0 ; $k<100 ; $k++){
+            $visite = new Visite();
+            $visite->setVille($faker->city)
+                    ->setPays($faker->country)
+                    ->setDatecreation($faker->dateTime)
+                    ->setTempmin($faker->numberBetween(-20, 10))
+                    ->setTempmax($faker->numberBetween(10, 40))
+                    ->setNote($faker->numberBetween(0, 20))
+                    ->setAvis($faker->sentence(4, true));
+            //enregistrement de l'objet
+            $manager->persist($visite);
+
+        }
+        $manager->flush();
+    }
+}
